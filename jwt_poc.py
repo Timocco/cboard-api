2f67906d7b1e392921d7cdcee1f7f3ec6162bdd6
Joshua Moyers <josh@restoreskills.com>
2023-12-07 11:53:00 -0500

    Changing ports to not interfere with keycloak

--- jwt_poc.py
+++ jwt_poc.py
@@ -1,17 +1,18 @@
 import jwt
+
 private_key = None
 with open("./private.key", mode="rb") as pk_file:
     private_key = pk_file.read()
 payload = {
-    "data1": 'Data 1',
-    "data2": 'Data 2',
-    "data3": 'Data 3',
-    "data4": 'Data 4',
+    "data1": "Data 1",
+    "data2": "Data 2",
+    "data3": "Data 3",
+    "data4": "Data 4",
     "iat": 1650116283,
     "exp": 1650159483,
-    "aud": 'http://localhost:8088',
-    "iss": 'RESTORE-Skills',
-    "sub": 'user@restoreskills.com'
+    "aud": "http://localhost:3088",
+    "iss": "RESTORE-Skills",
+    "sub": "user@restoreskills.com",
 }
 
 token = jwt.encode(payload, private_key, algorithm="RS256")
